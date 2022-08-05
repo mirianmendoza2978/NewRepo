254ee27f627bec2cdde36aae5dc5a1faa9a5a945
MIRIAN <MIRIAN@MIRIAN>
2022-08-05 16:46:20 -0500

    Agregar archivos de proyecto.

---
+++ Frm_Pago_Empleados.Designer.cs
@@ -0,0 +1,415 @@
+﻿
+namespace ControlPagoEmpleados
+{
+    partial class Frm_Pago_Empleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtHijos = new System.Windows.Forms.TextBox();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader10 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader11 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader12 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtHijos);
+            this.groupBox1.Controls.Add(this.txtEmpleado);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 69);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(435, 112);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DATOS DE EMPLEADO";
+            // 
+            // txtHijos
+            // 
+            this.txtHijos.Location = new System.Drawing.Point(328, 59);
+            this.txtHijos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtHijos.Name = "txtHijos";
+            this.txtHijos.Size = new System.Drawing.Size(89, 22);
+            this.txtHijos.TabIndex = 3;
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Location = new System.Drawing.Point(13, 59);
+            this.txtEmpleado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(272, 22);
+            this.txtEmpleado.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nº DE HIJOS";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 37);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EMPLEADO";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Location = new System.Drawing.Point(475, 71);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(631, 110);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DATOS DE PLANILLA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(435, 37);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "FECHA DE ING.";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(439, 57);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(156, 22);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(232, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "CONDICION";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "AREA";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(236, 53);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(28, 53);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9,
+            this.columnHeader10});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(16, 251);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1027, 180);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "EMPLEADO";
+            this.columnHeader1.Width = 81;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "HIJOS";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "AREA";
+            this.columnHeader3.Width = 61;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "CONDICION";
+            this.columnHeader4.Width = 75;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "TIEMPO DE SERV.";
+            this.columnHeader5.Width = 106;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "SUELDO BASE";
+            this.columnHeader6.Width = 90;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "MOVILIDAD";
+            this.columnHeader7.Width = 77;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "ASIGNACION";
+            this.columnHeader8.Width = 82;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "DESCUENTOS";
+            this.columnHeader9.Width = 86;
+            // 
+            // columnHeader10
+            // 
+            this.columnHeader10.Text = "NETO A COBRAR";
+            this.columnHeader10.Width = 104;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 200);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "REGISTRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(281, 11);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(516, 31);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "CONTROL DE PAGOS DE EMPLEADOS";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(968, 454);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "CONSULTA 1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(968, 490);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 28);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "CONSULTA 2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(968, 526);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 25);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "CONSULTA 3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(968, 558);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 28);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "SALIR";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader11,
+            this.columnHeader12});
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(16, 454);
+            this.listView2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(923, 141);
+            this.listView2.TabIndex = 9;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader11
+            // 
+            this.columnHeader11.Text = "CONDICION";
+            this.columnHeader11.Width = 442;
+            // 
+            // columnHeader12
+            // 
+            this.columnHeader12.Text = "VALOR";
+            this.columnHeader12.Width = 266;
+            // 
+            // Frm_Pago_Empleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.IndianRed;
+            this.ClientSize = new System.Drawing.Size(1172, 633);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Frm_Pago_Empleados";
+            this.Load += new System.EventHandler(this.Frm_Pago_Empleados_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtHijos;
+        private System.Windows.Forms.TextBox txtEmpleado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ColumnHeader columnHeader10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader11;
+        private System.Windows.Forms.ColumnHeader columnHeader12;
+    }
+}
---
+++ Frm_Pago_Empleados.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlPagoEmpleados
+{
+    public partial class Frm_Pago_Empleados : Form
+    {
+        double sueldo, movilidad;
+        int cPersonal, cComision;
+        double aPersonal, aComision;
+        int cVentas, cMarketing, cLogistica, cPrestamos;
+        double aVentas, aMarketing, aLogistica, aPrestamos;
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        DateTime fecha;
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            string condicion = comboBox2.Text;
+
+            //asignacion de sueldo de acorde a la condicion
+
+            if (condicion == "Personal") sueldo = 2500;
+            if (condicion == "Comision") sueldo = 1100;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Captura de los datos
+
+            string empleado = txtEmpleado.Text;
+            string hijos = txtHijos.Text;
+            string area = comboBox1.Text;
+            string condicion = comboBox2.Text;
+
+            // Conteo y acumulacion
+
+            switch (condicion)
+            {
+                case "Jefe": cPersonal++; aPersonal += sueldo; break;
+                case "Comision": cComision++; aComision += sueldo; break;
+            }
+
+            switch (area)
+            {
+                case "Ventas": cVentas++; aVentas += sueldo; break;
+                case "Marketing": cMarketing++; aMarketing += sueldo; break;
+                case "Logistica": cLogistica++; aLogistica += sueldo; break;
+                case "Prestamos": cPrestamos++; aPrestamos += sueldo; break;
+
+            }
+
+            // Realizando Calculo
+
+            double descuento = 0;
+
+            if (sueldo > 2500) descuento = (sueldo * 17) / 100;
+            if (sueldo > 1100) descuento = (sueldo * 17) / 100;
+
+
+            double neto = sueldo //+ MOVILIDAD + ASIGNACION
+                                 - descuento;
+
+            // imprimiendo los resultados
+
+            ListViewItem fila = new ListViewItem(empleado);
+            fila.SubItems.Add(hijos);
+            fila.SubItems.Add(area);
+            fila.SubItems.Add(condicion);
+            fila.SubItems.Add(fecha.ToString("d"));
+            fila.SubItems.Add(sueldo.ToString("C"));
+            //fila.SubItems.Add(movilidad.ToString("m"));
+            // fila.SubItems.Add(asignacion);
+            fila.SubItems.Add(descuento.ToString("C"));
+            fila.SubItems.Add(neto.ToString("C"));
+
+            listView1.Items.Add(fila);
+
+            // Imprimir estadisticas
+            listView2.Items.Clear();
+            string[] elementosFila = new string[4];
+            ListViewItem row;
+
+            //impresion de los datos de Ventas
+            elementosFila[0] = "Ventas";
+            elementosFila[1] = cVentas.ToString();
+            elementosFila[2] = aVentas.ToString("C");
+            row = new ListViewItem(elementosFila);
+            listView2.Items.Add(row);
+
+            //impresion de los datos de Marketing
+            elementosFila[0] = "Marketing";
+            elementosFila[1] = cMarketing.ToString();
+            elementosFila[2] = aMarketing.ToString("C");
+            row = new ListViewItem(elementosFila);
+            listView2.Items.Add(row);
+
+            //impresion de los datos de lOGISTICA
+            elementosFila[0] = "Logistica";
+            elementosFila[1] = cLogistica.ToString();
+            elementosFila[2] = aLogistica.ToString("C");
+            row = new ListViewItem(elementosFila);
+            listView2.Items.Add(row);
+
+            //impresion de los datos de prestamos
+            elementosFila[0] = "Prestamos";
+            elementosFila[1] = cPrestamos.ToString();
+            elementosFila[2] = aPrestamos.ToString("C");
+            row = new ListViewItem(elementosFila);
+            listView2.Items.Add(row);
+
+
+
+        }
+        private void llenarAreas()
+        {
+            comboBox1.Items.Add("Ventas");
+            comboBox1.Items.Add("Marketing");
+            comboBox1.Items.Add("Logistica");
+            comboBox1.Items.Add("Prestamos");
+        }
+        private void cargarCondicion() 
+        {
+            comboBox2.Items.Add("Personal");
+            comboBox2.Items.Add("Comision");
+        }
+ 
+        public Frm_Pago_Empleados()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Pago_Empleados_Load(object sender, EventArgs e)
+        {
+            llenarAreas();
+            cargarCondicion();
+           
+        }
+      
+    }
+}
